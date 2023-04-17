850e16ae390e44ee9cf0d6dcb0f463369359c45f
zilongzhang <zilongzhang@tencent.com>
2023-04-17 19:36:08 +0800

    add redis reader

--- readredis.go
+++ readredis.go
@@ -1,9 +1,42 @@
 package readredis
 
 import (
-    "fmt"
+	"fmt"
+	"github.com/go-redis/redis/v8"
 )
 
 func MyFunction() {
-    fmt.Println("Hello, this is readredis module!")
+	fmt.Println("Hello, this is readredis module!")
+}
+
+func ProcessData() {
+	// Connect to Redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	// Check the connection
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Error connecting to Redis: %v", err)
+	}
+
+	// Read data from Redis
+	key := "example_key"
+	value, err := rdb.Get(ctx, key).Result()
+	if err != nil {
+		log.Fatalf("Error getting value from Redis: %v", err)
+	}
+
+	// Process the data
+	result := process(value)
+
+	// Return the result
+	fmt.Printf("Processed result: %s\n", result)
+}
+
+func process(value string) string {
+	return "processed"
 }
